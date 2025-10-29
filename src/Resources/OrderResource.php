e265d899df705eb145c433093055caf21b087520
Alexandre <alexandre@devnco.fr>
2025-10-29 13:59:08 +0100

    Add findByInternalId

--- src/Resources/OrderResource.php
+++ src/Resources/OrderResource.php
@@ -4,4 +4,10 @@ namespace Sportyneo\SDK\Resources;
 class OrderResource extends BaseResource
 {
     protected $endpoint = '/orders';
+
+    public function findByInternalId(int $internalId): ?array
+    {
+        $result = $this->client->get($this->endpoint, ['internal_id' => $internalId]);
+        return $result['data'][0] ?? null;
+    }
 }
