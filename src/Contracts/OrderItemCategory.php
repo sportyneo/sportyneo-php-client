06231efab9e9d2b8d0c0cb4a19ee37624cca1b16
Alexandre <alexandre@devnco.fr>
2025-12-01 16:09:10 +0100

    Add item category

---
+++ src/Contracts/OrderItemCategory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Sportyneo\SDK\Contracts;
+
+enum ItemCategory: int
+{
+    case LICENSES = 1;
+    case STAGES = 2;
+    case AUTRES = 3;
+    case BOUTIQUE = 4;
+    case PLANNING = 5;
+    case MERCHANDISING = 6;
+    public function isDiscountable(): bool
+    {
+        return match($this) {
+            self::LICENSES, self::STAGES => true,
+            default => false,
+        };
+    }
+
+    public function getLabel(): string
+    {
+        return match($this) {
+            self::LICENSES => 'Adhésions',
+            self::STAGES => 'Stages',
+            self::AUTRES => 'Autres',
+            self::BOUTIQUE => 'Boutique',
+            self::PLANNING => 'Planning',
+            self::MERCHANDISING => 'Merchandising',
+        };
+    }
+}
